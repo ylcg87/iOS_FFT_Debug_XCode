ada415e9c9450e80a67c8b04eab2d4f334aae312
Liang Ge <tom.ge87@hotmail.com>
2018-01-25 17:20:03 +0800

    Minor change to plotAudioFrame

--- plotAudioFrame.py
+++ plotAudioFrame.py
@@ -2,7 +2,7 @@
 # Date: 2018-1-25
 # Audio Strings LLC Copyright(c) 2018
 
-# Record data from Xcode using this command:
+# Record audioFrame data from Xcode using this command:
 # memory read -t float -c2048 &audioFrame[0] --force
 # note: change 2048 to the exact audioFrame length
 
@@ -24,5 +24,13 @@ lineStr = fileID.readline()
 while lineStr != '':
 	audioFrame = np.append(audioFrame, float(lineStr))
 	lineStr = fileID.readline()
-	
+
+# Display audioFrame length	
 print 'audioFrame length = ', len(audioFrame)
+
+# Plot audioFrame data
+plt.figure(1, figsize=(16, 8))
+plt.plot(audioFrame)
+plt.title('audioFrame')
+
+plt.show()
